0ed3594f8060bf69c86bcebf7dcc758217144e48
Your Name <1031729253@qq.com>
2023-03-20 22:01:44 +0800

    第二个实验

--- kernel/bio.c
+++ kernel/bio.c
@@ -30,25 +30,33 @@ struct {
   // Linked list of all buffers, through prev/next.
   // Sorted by how recently the buffer was used.
   // head.next is most recent, head.prev is least.
-  struct buf head;
 } bcache;
+
+struct {
+  struct spinlock lock;
+  struct buf head;
+} bucket[13];
 
 void
 binit(void)
 {
-  struct buf *b;
-
+  struct buf* b;
+  int i;
   initlock(&bcache.lock, "bcache");
-
-  // Create linked list of buffers
-  bcache.head.prev = &bcache.head;
-  bcache.head.next = &bcache.head;
-  for(b = bcache.buf; b < bcache.buf+NBUF; b++){
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
+  for(int i = 0; i < 13; i++){
+    initlock(&bucket[i].lock, "bcache");//全部都进行初始化为bcache
+    bucket[i].head.next = &bucket[i].head;
+    bucket[i].head.prev = &bucket[i].head;
+    bucket[i].head.id = i;
+  }
+  for(b = bcache.buf,i = 0; b < bcache.buf + NBUF; b++,i++){
+    int j = i % 13;
+    b->next = bucket[j].head.next;
+    b->prev = &bucket[j].head;
     initsleeplock(&b->lock, "buffer");
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+    b->id = j;
+    bucket[j].head.next->prev = b;
+    bucket[j].head.next = b;
   }
 }
 
@@ -59,28 +67,58 @@ static struct buf*
 bget(uint dev, uint blockno)
 {
   struct buf *b;
-
-  acquire(&bcache.lock);
+  int id = blockno % 13;
+  acquire(&bucket[id].lock);
 
   // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
+  struct buf* tmp = 0;
+  for(b = bucket[id].head.next; b != &bucket[id].head; b = b->next){
     if(b->dev == dev && b->blockno == blockno){
       b->refcnt++;
-      release(&bcache.lock);
+      release(&bucket[id].lock);
       acquiresleep(&b->lock);
       return b;
+    }
+    else if(b->refcnt == 0){
+      tmp = b;
     }
   }
 
   // Not cached.
   // Recycle the least recently used (LRU) unused buffer.
-  for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
-    if(b->refcnt == 0) {
+  // 回收资源,回收一些已经是被释放了之后的资源
+
+  if(tmp != 0) {
+    tmp->dev = dev;
+    tmp->blockno = blockno;
+    tmp->valid = 0;
+    tmp->refcnt = 1;
+    release(&bucket[id].lock);
+    acquiresleep(&tmp->lock);
+    return tmp;
+  }
+
+  //需要对别的bucket进行抢用
+  acquire(&bcache.lock);
+  for(b = bcache.buf; b < (bcache.buf + NBUF); b += 1){
+    if(b->refcnt == 0){
+      //开始进行释放
+      int idold = b->id;
+      acquire(&bucket[idold].lock);
+      b->next->prev = b->prev;
+      b->prev->next = b->next;
+      b->next = bucket[id].head.next;
+      b->prev = &bucket[id].head;
+      bucket[id].head.next->prev = b;
+      bucket[id].head.next = b;
+      b->id = id;
       b->dev = dev;
       b->blockno = blockno;
       b->valid = 0;
       b->refcnt = 1;
+      release(&bucket[idold].lock);
       release(&bcache.lock);
+      release(&bucket[id].lock);
       acquiresleep(&b->lock);
       return b;
     }
@@ -121,33 +159,34 @@ brelse(struct buf *b)
 
   releasesleep(&b->lock);
 
-  acquire(&bcache.lock);
+  int id = b->id;
+  acquire(&bucket[id].lock);
   b->refcnt--;
   if (b->refcnt == 0) {
     // no one is waiting for it.
     b->next->prev = b->prev;
     b->prev->next = b->next;
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+    b->next = bucket[id].head.next;
+    b->prev = &bucket[id].head;
+    bucket[id].head.next->prev = b;
+    bucket[id].head.next = b;
   }
   
-  release(&bcache.lock);
+  release(&bucket[id].lock);
 }
 
 void
 bpin(struct buf *b) {
-  acquire(&bcache.lock);
+  acquire(&bucket[b->id].lock);
   b->refcnt++;
-  release(&bcache.lock);
+  release(&bucket[b->id].lock);
 }
 
 void
 bunpin(struct buf *b) {
-  acquire(&bcache.lock);
+  acquire(&bucket[b->id].lock);
   b->refcnt--;
-  release(&bcache.lock);
+  release(&bucket[b->id].lock);
 }
 
 
--- kernel/buf.h
+++ kernel/buf.h
@@ -1,4 +1,5 @@
 struct buf {
+  uint id;
   int valid;   // has data been read from disk?
   int disk;    // does disk "own" buf?
   uint dev;
